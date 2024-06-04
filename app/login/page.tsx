9ada46f55410103a92e66d30968d18410cc649a0
Andre <andrebp1020@gmail.com>
2024-06-03 22:20:16 -0500

    update login page imports

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,7 @@ import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/utils/cn";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import {IconBrandGoogle} from "@tabler/icons-react";
 
  const LoginPage = () => {
 
